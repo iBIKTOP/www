9e263ae3ae23f712dace96b37a2b2b76b480e391
iBIKTOP <biktopbb@gmail.com>
2017-03-02 21:00:46 +0200

    old

---
+++ header.php
@@ -0,0 +1,10 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title><?=$title?></title>
+	<!--<?=$title?> это тоже самое что и 
+		<?php echo $title;?>-->
+</head>
+<body>
+<p>Шапка</p>
